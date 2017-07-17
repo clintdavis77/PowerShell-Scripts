3cd9a58192ca7cfb7069f85cf52fc7f9197f55ca
Clint Davis <clint@clintdavis.us>
2017-07-16 22:29:45 -0400

    Add new script that moves files into directory with name of file

---
+++ move files into own directory.ps1
@@ -0,0 +1,17 @@
+﻿$folderToScan = "\\192.168.1.4\Root\something"
+ 
+$files = Get-ChildItem $folderToScan -File
+
+for ($i=0; $i -lt $files.Count; $i++) {
+    Write-Host($files[$i].BaseName)
+
+    $testPath = $folderToScan + "\" + $files[$i].BaseName
+    $testResult = Test-Path $testPath
+
+    If ($testResult -eq $false){
+        Write-Host("Create")
+        New-Item $testPath -ItemType directory
+    }
+
+    Move-Item $files[$i].FullName $testPath
+}
